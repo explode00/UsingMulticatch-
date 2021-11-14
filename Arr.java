08c1d9b3b48d063a89318ede9a2fd85b7ae82cca
explode00 <94221253+explode00@users.noreply.github.com>
2021-11-14 22:47:19 +0530

    Add files via upload

---
+++ Arr.java
@@ -0,0 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Arr {
+    public static void main(String []args){
+        try{
+            Scanner s = new Scanner(System.in);
+            System.out.println("Enter the number of elements in array");
+            int a = s.nextInt();
+            int[] ar = new int[a];
+            System.out.println("Enter the elements");
+            for(int i=0;i<ar.length;i++)
+                ar[i] = s.nextInt();
+            System.out.println("Enter the index of the array element you want to access");
+            int b = s.nextInt();
+            System.out.println("The array element at index "+b+" is "+ar[b]);
+            System.out.println("The array element is successfully accessed.");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Exception is "+e);
+        }
+        catch(InputMismatchException e){
+            System.out.println("Exception is"+e);
+        }
+    }
+}
---
+++ Multicatch.java
@@ -0,0 +1,18 @@
+public class Multicatch {
+    public static void main(String[] args){
+        try{
+            int a = args.length;
+            System.out.println("a = " + a);
+            int b =42/a;
+            int c[] = {1};
+            c[42] = 99;
+        }
+        catch(ArithmeticException e){
+            System.out.println("Divide by zero : "+ e);
+        }
+        catch(ArrayIndexOutOfBoundsException  e){
+            System.out.println("Array index out of bounds : " + e);
+        }
+        System.out.println("After try/catch blocks.");
+    }
+}
